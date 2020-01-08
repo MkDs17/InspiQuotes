9259a331178bc3cc4589e06037920367a98964ba
MkDs17 <mickael.rassicot@gmail.com>
2020-01-08 15:39:43 +0100

    ajout setInterval pour relancer toutes les x secondes la fonction reLoadQuote + Supp console.log

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 // On code proprement en JS => module / objet
 let app = {
+  $quotesList: [],
+
   // Par convention on crée une méthode init
   init: function() {
     console.log('init');
       
     app.randomBackground();
     app.loadQuote();
-    //app.loadPhoto(); 
+    
+    setInterval(app.reLoadQuote, 6500);
 
-    $('button').on('click', app.addOneLike);
+    $('#likes').on('click', app.addOneLike);
+    $('#reload').on('click', app.reLoadQuote);
+  },
+  
+  // Recharge une citation
+  reLoadQuote: function() {
+    app.generateQuote();
   },
 
   randomBackground: function() {
@@ -16,50 +25,18 @@ let app = {
     $("#bg").backstretch([`${base}bg.jpg`, `${base}bg1.jpg`, `${base}bg2.jpg`, `${base}bg3.jpg`, `${base}bg4.jpg`],
     {duration: 5000, fade:1500});
   }, 
-  randomNumber: function(min, max) {
+
+  //Fonction permet la séléction d'un chiffre au hasard
+  randomNumber: function(min, max) {    
     min = Math.ceil(min);
     max = Math.floor(max);
-    number = Math.floor(Math.random() * (max - min +1)) + min;  
-    return number  
+    $randomNumber = Math.floor(Math.random() * (max - min +1)) + min;  
+    return $randomNumber;
   },
 
-
-  loadPhoto: function() {
-
-      photosList = []
-
-      $.ajax(
-          {
-              url: 'https://api.unsplash.com/photos',
-              method: 'GET',
-              dataType: 'json',
-              headers: { 'Authorization' : ''}
-          }
-      ).done(
-          function(response) 
-          {
-              res = response;
-              console.log(res);
-              $.each(res, function(i, photos) 
-              {
-                photosList.push(photos.links.html)
-              })
-
-              console.log(photosList);
-
-          }
-      ).fail(
-          function() 
-          {
-              alert('Ajax loading failed');
-          }
-      );
-
-  }, 
   loadQuote: function() {
-    $quotesList = []
-    $quoteNumb = []
-
+    $quotesList = app.$quotesList
+    
     $.ajax(
       {
         url: 'quotes.json',
@@ -69,38 +46,40 @@ let app = {
     ).done(
       function(response) 
       {
-        res = response;
-        //console.log(res);
+        res = response;        
         $.each(res, function(i, quote) 
         {
-          $quotesList.push(quote)
-          //$quoteNumb.push(i)
+          $quotesList.push(quote)          
         })
 
-        app.randomNumber(0, 1)
-        console.log(number)
-        app.generateQuote(number);
+        app.generateQuote();
+
       }
     ).fail(
         function() 
         {
-            alert('Ajax loading failed');
+          alert('Ajax loading failed');
         }
     );   
 
   },  
 
-  generateQuote: function(numb) {
+  generateQuote: function() {
 
-    $('#quote').attr('data-id', $quotesList[numb]['id'])
-    $('#quote #text').append($quotesList[numb]['text'])
-    $('#quote #number').append($quotesList[numb]['likes'])
-    $('#quote #author').append($quotesList[numb]['author'])
+    // Permet de réactiver le button like 
+    $('#likes').find('button').removeClass('disabled');
+
+    // Longueur de l'array -1 , vu que l'index commence à zéro et non 1 !
+    app.randomNumber(0, ($quotesList.length-1))    
+
+    // .hide().html().fadeIn() => permet de replacer valeur précedente avec effet fadeIn
+    $('#quote').attr('data-id', $quotesList[$randomNumber]['id'])
+    $('#quote #text').hide().html($quotesList[$randomNumber]['text']).fadeIn(1500)
+    $('#quote #number').html($quotesList[$randomNumber]['likes'])
+    $('#quote #authorName').html($quotesList[$randomNumber]['author'])
   },
   
   addOneLike: function(evt) {
-    
-    console.log(evt.target)
 
     // permet de récuperer le nombre de likes du bouton sur lequel on clique
     $numberToChange = $(evt.target).find('#number').text()
@@ -110,13 +89,9 @@ let app = {
     $newNumber++
   
     $(evt.target).find('#number').text($newNumber)
-
-    $('#likes').find('button').addClass('disabled');
-
-    console.log($newNumber)
+    $('#likes').find('button').addClass('disabled')
 
   }
-
 
 };
   
